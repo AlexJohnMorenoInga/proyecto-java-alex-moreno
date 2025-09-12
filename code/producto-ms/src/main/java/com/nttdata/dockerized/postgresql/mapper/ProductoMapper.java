418f0a30f66f9c40ab832d25e742c8bc6b3f2944
Alex John Moreno Inga <amorenoinga@gmail.com>
2025-09-11 20:45:51 -0500

    Primer avance: configuración de OpenFeign y ajustes de base de datos

--- code/producto-ms/src/main/java/com/nttdata/dockerized/postgresql/mapper/ProductoMapper.java
+++ code/producto-ms/src/main/java/com/nttdata/dockerized/postgresql/mapper/ProductoMapper.java
@@ -1,6 +1,7 @@
 package com.nttdata.dockerized.postgresql.mapper;
 
 import com.nttdata.dockerized.postgresql.model.dto.ProductoDTO;
+import com.nttdata.dockerized.postgresql.model.dto.ProductoDTOb;
 import com.nttdata.dockerized.postgresql.model.entity.Producto;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -11,5 +12,7 @@ public interface ProductoMapper {
     ProductoMapper INSTANCE = Mappers.getMapper(ProductoMapper.class);
 
     Producto toProducto(ProductoDTO productoDTO);
+
+    ProductoDTOb toProductoDTOb(Producto producto);
 
 }
